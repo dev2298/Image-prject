b98608d52ac0431834a474aa15dba0ac0f565c90
DEV S <iamdevansh496@gmail.com>
2019-03-20 15:13:44 +0530

    first commit

---
+++ canny.m
@@ -0,0 +1,10 @@
+clc;
+clear all;
+close all;
+I = imread('C:\Users\DEV\Desktop\retina.jpg');
+grayeye1 = rgb2gray(I);
+dgrayeye1 = imadjust(grayeye1,[0.1 0.9],[]);
+se = strel('disk',1);
+cannyeye = edge(dgrayeye1,'canny',0.15);
+dilate = imdilate(cannyeye,se);
+figure,imshow(dilate);title('Extracted Blood Vessels');
---
+++ green.m
@@ -0,0 +1,31 @@
+clear all;
+close all;
+clc;
+I=imread('C:\Users\DEV\Desktop\diabetic.jpg');
+figure,imshow(I);
+I = imresize(I,[500,700]);
+greenc = I(:,:,2);
+ginv = imcomplement(greenc);
+adahist = adapthisteq(ginv);
+se = strel('ball',8,8);
+gopen = imopen(adahist,se);
+godisk = adahist-gopen;
+medfilt = medfilt2(godisk);
+background = imopen(medfilt,strel('disk',15));
+I2 = medfilt-background;
+I3 = imadjust(I2);
+level = graythresh(I3);
+bw = im2bw(I3,level);
+bw = bwareaopen(bw,30);
+figure,imshow(bw);
+wname = 'sym4';
+[CA,CH,CV,CD] = dwt2(bw,wname,'mode','per');
+figure,imshow(CA),title('Approximate');
+b = bwboundaries(bw);
+figure,imshow(I)
+hold on
+for k = 1:numel(b)
+    plot(b{k}(:,2),b{k}(:,1),'b','Linewidth',1)
+end
+
+
